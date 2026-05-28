2cd5d5621491be2a2726aa2d0d67e2b6fe4ca1a0
Maurício-Siqueira <mauricio.rsiqueira009@gmail.com>
2026-05-28 19:56:01 -0300

    Atualização do projeto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log("dados")
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+   
+let campoPesquisa = document.getElementById
+("campo-pesquisa").value
+
+// Se campoPesquisa for uma String sem nada
+if (campoPesquisa == "") {
+  section.innerHTML = "<p>Nada foi encontrado. Sem informações em busca</p>"  
+  return   
+}
+
+if (campoPesquisa == " ") {
+  section.innerHTML = "<p>Nada foi encontrado. Sem informações em busca</p>"  
+  return   
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    // Itera sobre cada item de dados e constrói o HTML para cada resultado
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      // Se titulo includes campoPesquisa
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        // Cria um novo elemento
+        resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <p> Ano de lançamento ${dado.ano}</p>
+          <p> Diretor: ${dado.diretor}</p>
+          <p> Elenco: ${dado.elenco}</p>
+          <p> Genero: ${dado.genero}</p>
+          <a href=${dado.link} target="_blank">Mais informações sobre o filme</a>
+        </div>
+      `;
+      }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada Foi encontrado</p>"
+    }
+    // Atribui o HTML gerado para o conteúdo da seção
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo : "Batman Begins",
+        descricao : "um jovem abalado pela trágica morte de seus pais. Em busca de justiça e vingança, ele embarca em uma jornada épica pelo mundo, onde se submete a um rigoroso treinamento físico e mental. Ao retornar à sua cidade natal, Gotham City, um lugar corrompido pela criminalidade",
+        link : "https://pt.wikipedia.org/wiki/Batman_Begins",
+        ano: "2005",
+        diretor: "Christopher Nolan",
+        elenco: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes, Gary Oldman, Cillian Murphy, Morgan Freeman",
+        genero: "Ação/Crime",
+    },
+    {
+        titulo : "Batman: O Cavaleiro das Trevas",
+        descricao : "Coringa, um anarquista psicopata, semeia o caos em Gotham City, forçando Batman a enfrentar seus limites enquanto tenta deter o criminoso mais perigoso que já enfrentou",
+        ano: "2008",
+        diretor: "Christopher Nolan",
+        elenco: "Christian Bale, Heath Ledger, Aaron Eckhart",
+        genero: "Ação",
+        link: "https://pt.wikipedia.org/wiki/Batman:_O_Cavaleiro_das_Trevas"
+    },
+    {
+        titulo : "Batman: O Cavaleiro das Trevas Ressurge",
+        descricao : "Oito anos após a morte de Harvey Dent, Gotham City está pacificada. Mas quando um novo vilão surge, Batman é forçado a sair de sua aposentadoria para salvar a cidade.",
+        ano: "2012",
+        diretor: "Christopher Nolan",
+        elenco: "Christian Bale, Anne Hathaway, Tom Hardy, Gary Oldman, Joseph Gordon-Levitt, Marion Cotillard",
+        genero: "Ação,Drama",
+        link : "https://pt.wikipedia.org/wiki/The_Dark_Knight_Rises"
+    }
+];
